20da0108baa69a01173e7bfa4ef4ca11b7e3a151
Satyajit Ghana <satyajitghana7@gmail.com>
2017-10-19 17:34:56 +0530

    added a crude Class Message

--- app/src/main/java/com/example/shadowleaf/rums/MainActivity.java
+++ app/src/main/java/com/example/shadowleaf/rums/MainActivity.java
@@ -5,6 +5,32 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.util.Log;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
+ * Message Class
+ * Author : Satyajit Ghana
+ * Updated 19-October-2017
+ */
+class Message {
+    private String message;
+    private String messageSender;
+    private String messageReciever;
+    private Date date;
+
+    Message(String message, String sender, String reciever) {
+        this.message = message;
+        this.date = new Date();
+        this.messageReciever = reciever;
+        this.messageSender = sender;
+    }
+
+    Message getMessage(Message message) {
+        return message;
+    }
+}
 
 public class        MainActivity extends AppCompatActivity {
 
